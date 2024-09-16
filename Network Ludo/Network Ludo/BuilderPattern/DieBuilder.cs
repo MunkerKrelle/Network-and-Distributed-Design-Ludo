8feff90b3b16cd3ace3ab36d971adbad6caa6f33
JeppeKjeldal <jeppekjeldal95@gmail.com>
2024-09-16 11:42:55 +0200

    DiceBuilder

--- Network Ludo/Network Ludo/BuilderPattern/DieBuilder.cs
+++ Network Ludo/Network Ludo/BuilderPattern/DieBuilder.cs
@@ -1,4 +1,6 @@
 ﻿using ComponentPattern;
+using Microsoft.Xna.Framework.Graphics;
+using Network_Ludo.ComponentPattern;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,14 +11,47 @@ namespace Network_Ludo.BuilderPattern
 {
     internal class DieBuilder : IBuilder
     {
+        private GameObject gameObject;
+
         public void BuildGameObject()
         {
-            throw new NotImplementedException();
+            gameObject = new GameObject();
+
+            BuildComponents();
+        }
+
+        private void BuildComponents()
+        {
+            gameObject.AddComponent<Die>();
+            gameObject.AddComponent<SpriteRenderer>();
+
+            Animator animator = gameObject.AddComponent<Animator>();
+            animator.AddAnimation(BuildAnimation("Roll", new string[] { "Side1", "Side2", "Side3", "Side4", "Side5", "Side6" }));
+        }
+
+        /// <summary>
+        /// Bygger en animation ved at indlæse de nødvendige sprites.
+        /// </summary>
+        /// <param name="animationName">Navnet på animationen.</param>
+        /// <param name="spriteNames">En række af sprite-navne, der udgør animationen.</param>
+        /// <returns>Den opbyggede animation.</returns>
+        private Animation BuildAnimation(string animationName, string[] spriteNames)
+        {
+            Texture2D[] sprites = new Texture2D[spriteNames.Length];
+
+            for (int i = 0; i < sprites.Length; i++)
+            {
+                sprites[i] = GameWorld.Instance.Content.Load<Texture2D>(spriteNames[i]);
+            }
+
+            Animation animation = new Animation(animationName, sprites, 5);
+
+            return animation;
         }
 
         public GameObject GetResult()
         {
-            throw new NotImplementedException();
+            return gameObject;
         }
     }
 }
