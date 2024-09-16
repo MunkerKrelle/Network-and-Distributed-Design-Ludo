6e7eefac041e0620c36752262fb97822a509d875
JeppeKjeldal <jeppekjeldal95@gmail.com>
2024-09-16 09:54:39 +0200

    DIE DIE DIE DIE DIE DIE DIE DIE

---
+++ Network Ludo/Network Ludo/ComponentPattern/Die.cs
@@ -0,0 +1,41 @@
+﻿using ComponentPattern;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network_Ludo.ComponentPattern
+{
+    internal class Die : Component
+    {
+        private Random rnd;
+        private int roll;
+        private Animator animator;
+
+        public Die(GameObject gameObject) : base(gameObject)
+        {
+        }
+
+        public override void Awake()
+        {
+            base.Awake();
+        }
+
+        public override void Start()
+        {
+            base.Start();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public void Roll()
+        {
+
+        }
+    }
+}
